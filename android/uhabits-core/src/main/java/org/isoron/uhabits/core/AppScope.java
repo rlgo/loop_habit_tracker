5f5800f165e3504fa228934f7e5233d7c2304e7e
rlgo <alexfang10@gmail.com>
2021-01-25 12:13:37 +0800

    Release version v2.0.0

--- android/uhabits-core/src/main/java/org/isoron/uhabits/core/AppScope.java
+++ android/uhabits-core/src/main/java/org/isoron/uhabits/core/AppScope.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 √Ålinson Santos Xavier <isoron@gmail.com>
+ * Copyright (C) 2016 ¡linson Santos Xavier <isoron@gmail.com>
  *
  * This file is part of Loop Habit Tracker.
  *
